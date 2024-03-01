327d2a2d926cebb4d7934a5e3d435df8843817cc
David Rooney <91282834+droonee@users.noreply.github.com>
2024-03-01 10:44:35 -0500

    Update tracer_auto.js

--- nodejs/tracer_auto.js
+++ nodejs/tracer_auto.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// opentelemetry packages used to create tracer provider, instrumentation, and exporter for application
 const opentelemetry = require("@opentelemetry/api");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { Resource } = require("@opentelemetry/resources");
@@ -9,11 +8,11 @@ const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-proto");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 
 module.exports = (serviceName) => {
   const resources = new Resource({
     "service.name": serviceName,
+    application: "nodejs-app",
   });
 
   const provider = new NodeTracerProvider({ resource: resources });
@@ -24,16 +23,11 @@ module.exports = (serviceName) => {
 
   const exporter = new OTLPTraceExporter(exporterOptions);
   provider.addSpanProcessor(new BatchSpanProcessor(exporter));
-
   provider.register();
 
   registerInstrumentations({
-    instrumentations: [
-      getNodeAutoInstrumentations({
-        "@opentelemetry/instrumentation-http": {},
-      }),
-    ],
+    instrumentations: [new HttpInstrumentation()],
   });
 
-  return opentelemetry.trace.getTracer("http-example");
+  return opentelemetry.trace.getTracer("nodejs-http-example");
 };
